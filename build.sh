8c74d07baa956c9ab29f0fb7ddbba3e3cb2bbdc5
chenh <1433255094@qq.com>
2024-01-09 23:08:25 +0800

    feat: update config

--- build.sh
+++ build.sh
@@ -5,12 +5,17 @@ BUILD_ROOT_RELEASE=2022.08.1
 mkdir -p source
 mkdir -p dist
 
+patch_source() {
+    patch source/buildroot-${BUILD_ROOT_RELEASE}/Makefile source-patch/Makefile.patch
+}
+
 get_source() {
     (
         cd source
         wget -c http://buildroot.org/downloads/buildroot-${BUILD_ROOT_RELEASE}.tar.gz
         tar xzf buildroot-${BUILD_ROOT_RELEASE}.tar.gz
     )
+    patch_source
 }
 
 start_build() {
